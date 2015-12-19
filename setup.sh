a971a016341e19d55dba1e6b3e486efa7bf81731
Tomasz Klim <github@tomaszklim.pl>
2015-12-19 23:26:55 +0100

    updated setup

--- setup.sh
+++ setup.sh
@@ -12,21 +12,21 @@ JAVAFILE="jre-8u66-linux-x64"
 BUNDLEID="111741"
 
 
+if [ -d /opt/java ] && [ ! -h /opt/java ]; then
+	mv /opt/java /opt/java.disabled
+fi
+
 if [ ! -d /opt/$JAVADIR ]; then
 	DIR="`pwd`"
 	cd /opt
 	wget -O $JAVAFILE.tar.gz http://javadl.sun.com/webapps/download/AutoDL?BundleId=$BUNDLEID
 	tar xzf $JAVAFILE.tar.gz
 	rm -f $JAVAFILE.tar.gz
+	ln -sf $JAVADIR java
 	cd "$DIR"
 fi
 
-if [ ! -d /opt/java ]; then
-	ln -sf /opt/$JAVADIR /opt/java
-fi
-
-if ! grep -q /opt/java/bin /etc/environment; then
-	echo 'PATH="$PATH:/opt/java/bin"' >>/etc/environment
+if ! grep -q JAVA_HOME /etc/environment; then
 	echo 'JAVA_HOME="/opt/java"' >>/etc/environment
 	echo 'JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true"' >>/etc/environment
 fi
