e902bac651772f6eee283b73af12bd1274a876fd
Tomasz Klim <github@tomaszklim.pl>
2020-04-18 11:33:57 +0200

    removed outdated raspbian support

--- setup.sh
+++ setup.sh
@@ -27,13 +27,6 @@ elif [ "$arch" = "i586" ] || [ "$arch" = "i686" ]; then
 	JAVAFILE="jre-8u241-linux-i586"
 	URL="https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241524_1f5b5a70bf22433b84d0e960903adac8"
 
-# as of 2019-04-18, all releases (even earlier than 2019-04-15) require logging in, probably due to new Java 8 license
-#elif [ "$arch" = "armv7l" ]; then
-#
-#	PRODUCT="jdk"
-#	JAVAFILE="jdk-8u241-linux-arm32-vfp-hflt"
-#	URL="https://download.oracle.com/otn/java/jdk/8u241-b07/1f5b5a70bf22433b84d0e960903adac8/jdk-8u241-linux-arm32-vfp-hflt.tar.gz"
-
 else
 	echo "architecture $arch is not supported, skipping Java setup"
 	exit 1
@@ -58,13 +51,6 @@ fi
 
 if ! grep -q JAVA_HOME /etc/environment; then
 	echo 'JAVA_HOME="/opt/java"' >>/etc/environment
-fi
-
-# On Raspbian, this directory is provided by outdated package oracle-java8-jdk,
-# which however can't be simply uninstalled due to complicated dependencies.
-if [ "$arch" = "armv7l" ] && [ -d /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt ]; then
-	rm -rf /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
-	ln -s /opt/java /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
 fi
 
 if [ "`which java`" = "" ]; then
